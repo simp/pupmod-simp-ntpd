193570c3a2f926501e89e8558f96c777aaec1d0d
Trevor Vaughan <tvaughan@onyxpoint.com>
2021-02-24 09:02:00 -0500

    (SIMP-9417) Fix ntp::allow::rules
    Fixed:
      * Fix an issue where ntp::allow::rules was not being honored
      * Added tests for ntp::allow::rules
    
    SIMP-9417 #close

--- spec/defines/allow_spec.rb
+++ spec/defines/allow_spec.rb
@@ -9,13 +9,34 @@ describe 'ntpd::allow' do
 
         context 'with default parameters' do
           it { is_expected.to compile.with_all_deps }
-          it { is_expected.to create_concat__fragment("ntpd_#{title}.allow").with_content(<<-EOF.gsub(/^\s+/,'')
-            restrict 1.2.3.0 mask 255.255.255.0
-            restrict 3.4.5.6
-            EOF
-          )}
+          it do
+            is_expected.to create_concat__fragment("ntpd_#{title}.allow")
+              .with_content(<<~CONTENT
+                restrict 1.2.3.0 mask 255.255.255.0
+                restrict 3.4.5.6
+                CONTENT
+              )
+          end
+
           it { is_expected.to_not contain_class('iptables')}
           it { is_expected.to_not contain_iptables__listen__udp('allow_ntp_test')}
+        end
+
+        context 'with rules set' do
+          let(:params) do
+            {
+              :rules => ['flake', 'nomodify']
+            }
+          end
+          it { is_expected.to_not contain_class('iptables') }
+          it do
+            is_expected.to create_concat__fragment("ntpd_#{title}.allow")
+              .with_content(<<~CONTENT
+                restrict 1.2.3.0 mask 255.255.255.0 flake nomodify
+                restrict 3.4.5.6 flake nomodify
+                CONTENT
+              )
+          end
         end
 
         context 'with firewall => true' do
