e7e574ae42709786b42e1151824a307f49d10317
Trevor Vaughan <tvaughan@onyxpoint.com>
2021-02-24 10:05:56 -0500

    (SIMP-9417) Fix ntp::allow::rules (#64)
    * (SIMP-9417) Fix ntp::allow::rules
    
    Fixed:
      * Fix an issue where ntp::allow::rules was not being honored
      * Added tests for ntp::allow::rules
      * Testing and formatting updates
      * Rubocop'd
      * Added JSON profiling custom rspec formatter
    
    SIMP-9417 #close

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'ntpd' do
@@ -10,7 +12,10 @@ describe 'ntpd' do
       context 'with default parmeters' do
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to create_concat('/etc/ntp.conf') }
-        it { is_expected.to create_concat__fragment('main_ntp_configuration').with_content(<<-EOF.gsub(/^[ ]+/,'')
+
+        it do
+          expect(subject).to create_concat__fragment('main_ntp_configuration')
+            .with_content(<<~MAIN_NTP_CONFIG,
             logconfig =syncall +clockall
 
             tinker panic 0
@@ -28,56 +33,70 @@ describe 'ntpd' do
             driftfile /var/lib/ntp/drift
             broadcastdelay 0.004
             disable monitor
-            EOF
-        ) }
+            MAIN_NTP_CONFIG
+                         )
+        end
 
-        it { is_expected.to_not contain_class('auditd')}
-        it { is_expected.to_not contain_ntpd__allow('simp_default_ntpd_allow') }
+        it { is_expected.not_to contain_class('auditd') }
+        it { is_expected.not_to contain_ntpd__allow('simp_default_ntpd_allow') }
         it { is_expected.to create_file('/etc/sysconfig/ntpd').with_content(%r{OPTIONS="-g"}) }
-        it { is_expected.to create_file('/etc/sysconfig/ntpdate').with_content(<<-EOF.gsub(/^\s+/,'')
-          # Configuration for the ntpdate script that runs at boot
-          # This file is managed by Puppet (module: ntpd)
-          # Options for ntpdate
-          OPTIONS="-p 2"
-          # Number of retries before giving up
-          RETRY=2
-          # Set to 'yes' to sync hw clock after successful ntpdate
-          SYNC_HWCLOCK=yes
-          EOF
-        ) }
+
+        it do
+          expect(subject).to create_file('/etc/sysconfig/ntpdate')
+            .with_content(<<~CONTENT,
+            # Configuration for the ntpdate script that runs at boot
+            # This file is managed by Puppet (module: ntpd)
+            # Options for ntpdate
+            OPTIONS="-p 2"
+            # Number of retries before giving up
+            RETRY=2
+            # Set to 'yes' to sync hw clock after successful ntpdate
+            SYNC_HWCLOCK=yes
+            CONTENT
+                         )
+        end
       end
 
-      context 'virtual' do
-        let(:facts) {
-          os_facts.merge({:virtual => 'kvm'})
-        }
+      context 'when virtual' do
+        let(:facts) do
+          os_facts.merge({ :virtual => 'kvm' })
+        end
 
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to create_concat__fragment('main_ntp_configuration').with_content(/tinker panic 0/) }
+        it { is_expected.to create_concat__fragment('main_ntp_configuration').with_content(%r{tinker panic 0}) }
       end
 
       context 'with auditd => true' do
-        let(:params){{ :auditd => true }}
+        let(:params) { { :auditd => true } }
 
         it { is_expected.to compile.with_all_deps }
         it { is_expected.to create_class('auditd') }
-        it { is_expected.to create_auditd__rule('ntp').with_content(<<-EOF.gsub(/^\s+/,'')
-            -w /etc/ntp.conf -p wa -k CFG_ntp
-            -w /etc/ntp/keys -p wa -k CFG_ntp
-          EOF
-        ) }
+
+        it do
+          expect(subject).to create_auditd__rule('ntp')
+            .with_content(<<~CONTENT,
+              -w /etc/ntp.conf -p wa -k CFG_ntp
+              -w /etc/ntp/keys -p wa -k CFG_ntp
+            CONTENT
+                         )
+        end
       end
 
       context 'with servers array' do
-        let(:params){{
-          'servers' => [
-            'time.bar.baz',
-            'time.other.net'
-          ]
-        }}
+        let(:params) do
+          {
+            'servers' => [
+              'time.bar.baz',
+              'time.other.net',
+            ]
+          }
+        end
 
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to create_concat__fragment('main_ntp_configuration').with_content(<<-EOF.gsub(/^[ ]+/,'')
+
+        it do
+          expect(subject).to create_concat__fragment('main_ntp_configuration')
+            .with_content(<<~CONTENT,
             logconfig =syncall +clockall
 
             tinker panic 0
@@ -96,32 +115,41 @@ describe 'ntpd' do
             driftfile /var/lib/ntp/drift
             broadcastdelay 0.004
             disable monitor
-            EOF
-        ) }
+            CONTENT
+                         )
+        end
 
-        it { is_expected.to create_file('/etc/ntp/step-tickers').with_content(<<-EOF.gsub(/^\s+/,'')
+        it do
+          expect(subject).to create_file('/etc/ntp/step-tickers')
+            .with_content(<<~CONTENT,
             # List of NTP servers used by the ntpdate service.
             # This file is managed by Puppet (module: ntpd)
             time.bar.baz
             time.other.net
-          EOF
-        ) }
+            CONTENT
+                         )
+        end
       end
 
       context 'with servers hash' do
-        let(:params){{
-          'servers' => {
-            'time.bar.baz' => ['prefer'],
-            'time.other.net' => []
-          },
-          'discard' => {
-            'average' => 3,
-            'minimum' => 0,
+        let(:params) do
+          {
+            'servers' => {
+              'time.bar.baz' => ['prefer'],
+              'time.other.net' => []
+            },
+            'discard' => {
+              'average' => 3,
+              'minimum' => 0
+            }
           }
-        }}
+        end
 
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to create_concat__fragment('main_ntp_configuration').with_content(<<-EOF.gsub(/^[ ]+/,'')
+
+        it do
+          expect(subject).to create_concat__fragment('main_ntp_configuration')
+            .with_content(<<~CONTENT,
             logconfig =syncall +clockall
 
             tinker panic 0
@@ -141,25 +169,34 @@ describe 'ntpd' do
             driftfile /var/lib/ntp/drift
             broadcastdelay 0.004
             disable monitor
-            EOF
-        ) }
+            CONTENT
+                         )
+        end
 
-        it { is_expected.to create_file('/etc/ntp/step-tickers').with_content(<<-EOF.gsub(/^\s+/,'')
+        it do
+          expect(subject).to create_file('/etc/ntp/step-tickers')
+            .with_content(<<~CONTENT,
             # List of NTP servers used by the ntpdate service.
             # This file is managed by Puppet (module: ntpd)
             time.bar.baz
             time.other.net
-          EOF
-        ) }
+            CONTENT
+                         )
+        end
       end
 
       context 'with extra content' do
-        let(:params){{
-          'extra_content' => "This is some\nextra content"
-        }}
+        let(:params) do
+          {
+            'extra_content' => "This is some\nextra content"
+          }
+        end
 
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to create_concat__fragment('main_ntp_configuration').with_content(<<-EOF.gsub(/^[ ]+/,'')
+
+        it do
+          expect(subject).to create_concat__fragment('main_ntp_configuration')
+            .with_content(<<~CONTENT,
             logconfig =syncall +clockall
 
             tinker panic 0
@@ -182,53 +219,65 @@ describe 'ntpd' do
             This is some
             extra content
             # End raw user content
-            EOF
-        ) }
+            CONTENT
+                         )
+        end
       end
 
       context 'with fully user-defined content' do
-        let(:params){{
-          'config_content' => 'This is all you get',
-          'extra_content' => "This is some\nextra content"
-        }}
+        let(:params) do
+          {
+            'config_content' => 'This is all you get',
+            'extra_content' => "This is some\nextra content"
+          }
+        end
 
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to create_concat__fragment('main_ntp_configuration').with_content(<<-EOF.gsub(/^[ ]+/,'')
+
+        it do
+          expect(subject).to create_concat__fragment('main_ntp_configuration')
+            .with_content(<<~CONTENT,
             # Begin user-defined configuration
             This is all you get
             # End user-defined configuration
-            EOF
-        ) }
+            CONTENT
+                         )
+        end
       end
 
       context 'with servers and vmware' do
-        let(:facts) {
-          os_facts.merge({:virtual => 'vmware'})
-        }
+        let(:facts) do
+          os_facts.merge({ :virtual => 'vmware' })
+        end
 
-        let(:params){{
-          'servers' => [
-            'time.bar.baz',
-            'time.other.net'
-          ]
-        }}
+        let(:params) do
+          {
+            'servers' => [
+              'time.bar.baz',
+              'time.other.net',
+            ]
+          }
+        end
 
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to_not create_concat__fragment('main_ntp_configuration').with_content(/server\s+127\.127\.1\.0/) }
-        it { is_expected.to_not create_concat__fragment('main_ntp_configuration').with_content(/fudge\s+127\.127\.1\.0\s+stratum/) }
+        it { is_expected.not_to create_concat__fragment('main_ntp_configuration').with_content(%r{server\s+127\.127\.1\.0}) }
+        it { is_expected.not_to create_concat__fragment('main_ntp_configuration').with_content(%r{fudge\s+127\.127\.1\.0\s+stratum}) }
       end
 
       context 'with logconfig empty' do
-        let(:params){{ :logconfig => [] }}
+        let(:params) { { :logconfig => [] } }
+
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to_not create_concat__fragment('main_ntp_configuration').with_content(/logconfig/) }
+        it { is_expected.not_to create_concat__fragment('main_ntp_configuration').with_content(%r{logconfig}) }
       end
 
       context 'when allowing remote connections' do
         context 'with just trusted_nets defined' do
-          let(:params){{
-            :trusted_nets => ['127.0.0.1', 'localhost']
-          }}
+          let(:params) do
+            {
+              :trusted_nets => ['127.0.0.1', 'localhost']
+            }
+          end
 
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to create_ntpd__allow('simp_default_ntpd_allow').with_rules(nil) }
@@ -236,10 +285,12 @@ describe 'ntpd' do
         end
 
         context 'with rules defined' do
-          let(:params){{
-            :trusted_nets => ['127.0.0.1', 'localhost'],
-            :default_restrict_rules => ['kod']
-          }}
+          let(:params) do
+            {
+              :trusted_nets => ['127.0.0.1', 'localhost'],
+              :default_restrict_rules => ['kod']
+            }
+          end
 
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to create_ntpd__allow('simp_default_ntpd_allow').with_rules(params[:default_restrict_rules]) }
@@ -248,25 +299,39 @@ describe 'ntpd' do
       end
 
       context 'with simp_options::ntp::servers set' do
-        let(:hieradata) {'simp_options_ntp_servers' }
+        let(:hieradata) { 'simp_options_ntp_servers' }
 
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to create_concat__fragment('main_ntp_configuration').with_content(
-          /server time.bar.baz prefer/ ) }
 
-        it { is_expected.to create_concat__fragment('main_ntp_configuration').with_content(
-          /server time.other.net minpoll 4 maxpoll 4 iburst/ ) }
+        it {
+          expect(subject).to create_concat__fragment('main_ntp_configuration').with_content(
+            %r{server time.bar.baz prefer},
+          )
+        }
+
+        it {
+          expect(subject).to create_concat__fragment('main_ntp_configuration').with_content(
+            %r{server time.other.net minpoll 4 maxpoll 4 iburst},
+          )
+        }
       end
 
       context 'with simp_options::ntpd::servers set' do
-        let(:hieradata) {'simp_options_ntpd_servers' }
+        let(:hieradata) { 'simp_options_ntpd_servers' }
 
         it { is_expected.to compile.with_all_deps }
-        it { is_expected.to create_concat__fragment('main_ntp_configuration').with_content(
-          /server time.foo.bar minpoll 4 maxpoll 4 iburst/ ) }
 
-        it { is_expected.to create_concat__fragment('main_ntp_configuration').with_content(
-          /server time.foo.baz prefer/ ) }
+        it {
+          expect(subject).to create_concat__fragment('main_ntp_configuration').with_content(
+            %r{server time.foo.bar minpoll 4 maxpoll 4 iburst},
+          )
+        }
+
+        it {
+          expect(subject).to create_concat__fragment('main_ntp_configuration').with_content(
+            %r{server time.foo.baz prefer},
+          )
+        }
       end
     end
   end
